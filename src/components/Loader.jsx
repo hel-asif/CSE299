2f3ba3db9d30201d41e3b1531abc997c9863167a
Abdullah Hel Asif <74124652+hel-asif@users.noreply.github.com>
2022-11-21 00:50:40 +0600

    Added a loading animation

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,15 @@
-const Loader = ({ title }) => (
+import {loader} from '../assets';
 
+
+const Loader = ({title}) => (
+  
   <div className="w-full flex justify-center items-center flex-col">
-    <h1 className="font-bold text-2xl text-white mt-2">
-      {title || 'Loading...'}
-    </h1>
-  </div>
+    
+   <img src={loader} alt="loader" className="w-32 h-32 object-contain"/> 
+   <h1 className="font-bold text-2xl text-white mt-2">
+    {title || 'Loading...'}
+   </h1>
+    </div>
 );
 
 export default Loader;
