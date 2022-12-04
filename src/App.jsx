3c3d9c31e11442602718db1b972b481fb44c57b9
Asad-Simon <95439497+Asad-Simon@users.noreply.github.com>
2022-12-04 23:19:25 +0600

    Added music player to website

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { Discover} from './pages';
+import { Sidebar, MusicPlayer, TopPlay } from './components';
 const App = () => {
   const { activeSong } = useSelector((state) => state.player);
   return (
     <div className="relative flex">
-      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#3a965d]">
+      <Sidebar/>
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#0a4a22]">
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Routes>
@@ -13,15 +15,18 @@ const App = () => {
             </Routes>
           </div>
           <div className="xl:sticky relative top-0 h-fit">
+          <TopPlay />
           </div>
         </div>
       </div>
-      {activeSong?.title && (
-        <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup 
-        bg-gradient-to-br from-white/10 to-[#6bb085] backdrop-blur-lg rounded-t-3xl z-10">
+      {(
+        <div className="absolute h-28 bottom-0 left-0 right-0 flex  
+        bg-[#000000] backdrop rounded-t-2xl z-10">
+         <MusicPlayer />
         </div>
       )}
     </div>
+    
   );
 };
 export default App;
