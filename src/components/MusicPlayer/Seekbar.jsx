4b0f09f25d8155ecfb535f03b6896ac0ca228c11
Abdullah Hel Asif <74124652+hel-asif@users.noreply.github.com>
2022-12-05 09:49:21 +0600

    Redesigned seek bar
    added seek forward and seek backward button

--- src/components/MusicPlayer/Seekbar.jsx
+++ src/components/MusicPlayer/Seekbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {TbPlayerTrackPrev, TbPlayerTrackNext} from 'react-icons/tb';
 
 const Seekbar = ({ value, min, max, onInput, setSeekTime, appTime }) => {
 
@@ -6,9 +7,7 @@ const Seekbar = ({ value, min, max, onInput, setSeekTime, appTime }) => {
 
   return (
     <div className="hidden sm:flex flex-row items-center">
-      <button type="button" onClick={() => setSeekTime(appTime - 5)} className="hidden lg:mr-4 lg:block text-white">
-        -
-      </button>
+       <TbPlayerTrackPrev size={20} color="#FFF" onClick={() => setSeekTime(appTime - 5)} className="hidden lg:mr-4 lg:block text-white"/>
       <p className="text-white">{value === 0 ? '0:00' : getTime(value)}</p>
       <input
         type="range"
@@ -20,9 +19,7 @@ const Seekbar = ({ value, min, max, onInput, setSeekTime, appTime }) => {
         className="md:block w-24 md:w-56 2xl:w-96 h-1 mx-4 2xl:mx-6 rounded-lg"
       />
       <p className="text-white">{max === 0 ? '0:00' : getTime(max)}</p>
-      <button type="button" onClick={() => setSeekTime(appTime + 5)} className="hidden lg:ml-4 lg:block text-white">
-        +
-      </button>
+      <TbPlayerTrackNext size={20} color="#FFF" onClick={() => setSeekTime(appTime - 5)} className="hidden lg:ml-4 lg:block text-white"/>
     </div>
   );
 };
