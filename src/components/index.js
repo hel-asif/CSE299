68b269ade2f62b14d0e3c32d2287dd953782d62a
Abdullah Hel Asif <74124652+hel-asif@users.noreply.github.com>
2022-12-11 11:16:06 +0600

    Added other classes to index

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,15 @@
 import SongCard from './SongCard';
 import Loader from './Loader';
 import Error from './Error';
+import Sidebar from './Sidebar';
+import MusicPlayer from './MusicPlayer';
+import TopPlay from './TopPlay';
 
 export {
   SongCard,
   Loader,
   Error,
+  Sidebar,
+  MusicPlayer,
+  TopPlay
 };
