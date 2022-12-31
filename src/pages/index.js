14e2ece39c39a572e982e4732f5cc0a269dd1baf
MarionMoon <73684725+MarionMoon@users.noreply.github.com>
2022-12-31 19:52:04 +0600

    Created search page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,11 @@
 import Discover from './Discover';
-
+import SongDetails from './SongDetails';
+import TopCharts from './TopCharts'
+import Search from './Search'
 
 export {
   Discover,
+  SongDetails,
+  TopCharts,
+  Search
 };
