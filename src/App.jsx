3c274e8b69944dec1ee475431d69849e8c9538a7
Abdullah Hel Asif <74124652+hel-asif@users.noreply.github.com>
2022-12-31 19:44:20 +0600

    Added routes to buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
-import { Discover} from './pages';
-import { Sidebar, MusicPlayer, TopPlay } from './components';
+import { Discover,Search,  SongDetails, TopCharts} from './pages';
+import { Sidebar,Searchbar, MusicPlayer, TopPlay } from './components';
 const App = () => {
   const { activeSong } = useSelector((state) => state.player);
   return (
     <div className="relative flex">
       <Sidebar/>
-      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#0a4a22]">
+      <div className="p-4 flex-1 flex flex-col bg-gradient-to-br from-black to-[#0a4a22]">
+      <Searchbar />
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Routes>
               <Route path="/" element={<Discover />} />
+              <Route path="/top-charts" element={<TopCharts />} />
+              <Route path="/songs/:songid" element={<SongDetails />} />
+              <Route path="/search/:searchTerm" element={<Search />} />
             </Routes>
           </div>
           <div className="xl:sticky relative top-0 h-fit">
-          <TopPlay />
+            <TopPlay/>
           </div>
         </div>
       </div>
